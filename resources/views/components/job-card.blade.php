ae50b26faf23a7fc50c1a7a00bfea906a7223c83
Keyliananda <info@piano-volz.de>
2024-08-12 14:56:15 +0200

    Day30-vor-Ende

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -3,18 +3,18 @@
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
-    <div class="py-8 ">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold">{{ $job->title }}</h3>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
         <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
             @foreach($job->tags as $tag)
-            <x-tag :$tag size="small" />
+                <x-tag :$tag size="small" />
             @endforeach
         </div>
-        <x-employer-logo :width="42"/>
 
+        <x-employer-logo :width="42" />
     </div>
 </x-panel>
